2a848cc8afaa65efa444e9b370e84c07d788569d
eduardohgo <eduardotol.770@gmail.com>
2025-12-02 08:29:41 -0600

    Update layout.js

--- pry_lapape/src/app/layout.js
+++ pry_lapape/src/app/layout.js
@@ -1,6 +1,6 @@
-// src/app/layout.js
 import "./globals.css";
 import AuthProvider from "@/components/AuthProvider";
+import ClarityTag from "@/components/ClarityTag"; // 👈 IMPORTANTE
 
 export const metadata = {
   title: "La Pape",
@@ -11,8 +11,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          {/* Inyecta el script de Clarity en TODAS las páginas */}
+          <ClarityTag />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
 }
+
