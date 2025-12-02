1e7426bba341f2368d26e84444bca1300b603508
eduardohgo <eduardotol.770@gmail.com>
2025-12-02 05:14:20 -0600

    Create head.js

---
+++ pry_lapape/src/app/head.js
@@ -0,0 +1,17 @@
+// src/app/head.js
+
+export default function Head() {
+  return (
+    <>
+      <title>La Pape</title>
+      <meta name="description" content="Papelería creativa" />
+
+      {/* 🔴 AQUÍ VA EL SCRIPT DE CONTENTSQUARE */}
+      <script
+        id="contentsquare-tag"
+        src="https://t.contentsquare.net/uxa/d6ed8b7dad4d.js"
+        async
+      ></script>
+    </>
+  );
+}
