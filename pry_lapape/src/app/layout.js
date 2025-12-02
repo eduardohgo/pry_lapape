aec6ccc5949e11b90512b54732179ed0c8a9c90d
eduardohgo <eduardotol.770@gmail.com>
2025-12-02 02:25:28 -0600

    Update layout.js

--- pry_lapape/src/app/layout.js
+++ pry_lapape/src/app/layout.js
@@ -1,6 +1,7 @@
 // src/app/layout.js
 import "./globals.css";
 import AuthProvider from "@/components/AuthProvider";
+import Script from "next/script"; // 👈 nuevo import
 
 export const metadata = {
   title: "La Pape",
@@ -10,9 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <Script
+          id="contentsquare-tag"
+          strategy="afterInteractive"
+          src="https://t.contentsquare.net/uxa/d6ed8b7dad4d4.js" // 👈 usa aquí exactamente la URL que te dio la página
+        />
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
 }
+
